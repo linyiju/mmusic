e49adfffb517a70f310aa322598a1627cea11700
linyiju <lynn8301@gmail.com>
2020-03-21 13:07:09 +0800

    Rename post table name #1

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 class Post(models.Model):
     author = models.CharField(_('作者'), max_length=100, unique=True)
     title = models.CharField(_('標題'), max_length=225)
-    contnet = models.TextField(verbose_name=_('內文'))
+    content = models.TextField(verbose_name=_('內文'))
     created_at = models.DateTimeField(_('建立時間'), auto_now_add=True)
     last_modified_at = models.DateTimeField(_('最後修改時間'), auto_now=True)
 
