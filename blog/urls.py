5a5a34d57d403006a9195e6c06374e2975a09299
linyiju <lynn8301@gmail.com>
2020-03-21 18:09:43 +0800

    Modify post page #1

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from blog import views
 from django.urls import path
+
+from blog import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>', views.post_detial, name='post_detial')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from blog.models import Post
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+
+from blog.models import Post
 
 
 def post_list(request):
     posts = Post.objects.filter(created_at__lte=timezone.now()).order_by('created_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detial(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detial.html', {'post': post})
