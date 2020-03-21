34b40dc400ed8449ab15bfdd0700cdbd5bec5834
linyiju <lynn8301@gmail.com>
2020-03-21 18:34:55 +0800

    Modify Post model author field #1

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
 class Post(models.Model):
-    author = models.CharField(_('作者'), max_length=100, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('作者'), on_delete=models.PROTECT, default='')
     title = models.CharField(_('標題'), max_length=225)
-    content = models.TextField(verbose_name=_('內文'))
-    created_at = models.DateTimeField(_('建立時間'), auto_now_add=True)
+    content = models.TextField(verbose_name=_('內文'), null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name=_('建立時間'), auto_now_add=True)
     last_modified_at = models.DateTimeField(_('最後修改時間'), auto_now=True)
 
     class Meta:
