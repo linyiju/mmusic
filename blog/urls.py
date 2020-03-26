336546364fc6d825bdb98460da43226e4343bbe3
linyiju <lynn8301@gmail.com>
2020-03-26 09:29:49 +0800

    Blog url create draft path #3

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,8 @@
-from blog.views import post_detail, post_edit, post_list, post_new
+from blog import views
 from django.urls import path
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
-    path('post/<int:pk>', post_detail, name='post_detail'),
-    path('post/new/', post_new, name='post_new'),
-    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
 ]
