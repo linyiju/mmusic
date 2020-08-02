08a4f122287eefa86323d813f03ff0985ebbf796
linyiju <lynn8301@gmail.com>
2020-08-02 16:18:27 +0800

    Create add comment views #7

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
-from blog.forms import PostForm
+from blog.forms import CommentForm, PostForm
 from blog.models import Post
 
 
@@ -78,3 +78,17 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
